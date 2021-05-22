76823abbfe585c57530f931332c3617c2da473be
Ken Seah <seahken@gmail.com>
2021-05-22 17:53:44 +1000

    added testCases for Property component

--- components/Property.vue
+++ components/Property.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-  </div>
+  <article class="property">
+    <div class="header" :style="headerStyles">
+      <img :src="propertyData.agency.logo" />
+    </div>
+    <div class="body">
+      <img :src="propertyData.mainImage" class="main-image" />
+    </div>
+    <div class="footer">
+      <p>{{ propertyData.price }}</p>
+    </div>
+  </article>
 </template>
 
 <script>
@@ -10,7 +19,44 @@ export default {
     propertyData: {
       type: Object,
       required: true
+    },
+    isResultsColumn: {
+      type: Boolean,
+      default: false
+    },
+    isSavedColumn: {
+      type: Boolean,
+      default: false
+    },
+    isPropertyAdded: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    headerStyles() {
+      const styles = []
+      const backgroundColor = this.propertyData.agency?.brandingColors?.primary
+      if (backgroundColor) styles.push({ backgroundColor })
+
+      return styles
     }
   }
 }
 </script>
+
+<style scoped>
+.property {
+  box-shadow: rgb(0 0 0 / 20%) 0px 0.0625rem 0.1875rem 0px;
+  border-radius: 0.1875rem;
+  background-color: rgb(255, 255, 255);
+}
+
+.header {
+  padding: 8px;
+}
+
+.main-image {
+  width: 100%;
+}
+</style>
--- test/Property.spec.js
+++ test/Property.spec.js
@@ -7,13 +7,53 @@ describe('App data', () => {
 
   const { results, saved } = JSON.parse(getMockDataTest())
 
-  // for results, on hover, see add property button
+  const testCases = [
+    {
+      title: 'for resutls, no see add property button',
+      isResultsColumn: true,
+      isHovering: false,
+      isPropertyAdded: false,
+      expectedSeeAddPropertyButton: false,
+      expectedSeeRemovePropertyButton: false
+    },
+    {
+      title: 'for resutls, on hover, see add property button',
+      isResultsColumn: true,
+      isHovering: true,
+      isPropertyAdded: false,
+      expectedSeeAddPropertyButton: true,
+      expectedSeeRemovePropertyButton: false
+    },
+    {
+      title: 'for results, on hover, if already added, see already added notification',
+      isResultsColumn: true,
+      isHovering: true,
+      isPropertyAdded: true,
+      expectedSeeAddPropertyButton: false,
+      expectedSeeAlreadyAddedNotification: true,
+      expectedSeeRemovePropertyButton: false
+    },
+    {
+      title: 'for saved property, no see remove property button',
+      isResultsColumn: false,
+      isSavedColumn: true,
+      isHovering: false,
+      expectedSeeAddPropertyButton: false,
+      expectedSeeAlreadyAddedNotification: false,
+      expectedSeeRemovePropertyButton: false
+    },
+    {
+      title: 'for saved property, on hover, see remove property button',
+      isResultsColumn: false,
+      isSavedColumn: true,
+      isHovering: true,
+      expectedSeeAddPropertyButton: false,
+      expectedSeeAlreadyAddedNotification: false,
+      expectedSeeRemovePropertyButton: true
+    }
+  ]
 
-  // for results, on hover, if already added, ???
-
-  // for resutls, see no add property button
-
-  // for saved property, on hover, see remove property button
-
-  // for saved property, see no remove property button
+  testCases.forEach((testCase, i) => {
+    
+  })
 })
